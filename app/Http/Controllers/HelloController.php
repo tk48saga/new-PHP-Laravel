55500b3a4bc916221235e3042e91854ed3bb3979
tk48saga <takahiro.koga@arsaga.jp>
2024-02-22 14:16:48 +0900

    コントローラの変更(編集)
    Hello, Laravel!!

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //新規追加のメソッド
     public function index(){
-        return "hello, world";
+        //変更箇所
+        $message = "Hello Laravel!!";
+        return view('hello',['message' => $message]);
     }
 }
