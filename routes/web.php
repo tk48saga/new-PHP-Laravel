770a9db0180bd79690e1abd9c2b4a2da651e264a
tk48saga <takahiro.koga@arsaga.jp>
2024-02-22 16:19:49 +0900

    web.phpにroute追加
    routeにviewが返ってくるURLを追加

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register',[\App\Http\Controllers\UserController::class,'showRegister']);
